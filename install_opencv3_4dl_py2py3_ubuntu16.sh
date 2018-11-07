513be97dc2ea19f54190d6f1c0810e3c291ec36a
Chibuike Okpaluba <chibike@users.noreply.github.com>
2018-11-07 19:29:23 +0000

    Update install_opencv3_4dl_py2py3_ubuntu16.sh

--- install_opencv3_4dl_py2py3_ubuntu16.sh
+++ install_opencv3_4dl_py2py3_ubuntu16.sh
@@ -113,3 +113,29 @@ ln -s /usr/local/lib/python2.7/dist-packages/cv2.so cv2.so
 
 cd ~/.virtualenvs/dl_py3/lib/python3.5/site-packages
 ln -s /usr/local/lib/python3.5/dist-packages/cv2.cpython-35m-x86_64-linux-gnu.so cv2.so
+
+
+#Step 5.4: Create symlink in virtual environment
+
+#Depending upon Python version you have, paths would be different. OpenCV’s Python binary (cv2.so) can be installed either in directory site-packages or dist-packages. Use the following command to #find out the correct location on your machine.
+
+#find /usr/local/lib/ -type f -name "cv2*.so"
+#It should output paths similar to one of these (or two in case OpenCV was compiled for both Python2 and Python3):
+
+############ For Python 2 ############
+## binary installed in dist-packages
+#/usr/local/lib/python2.6/dist-packages/cv2.so
+#/usr/local/lib/python2.7/dist-packages/cv2.so
+## binary installed in site-packages
+#/usr/local/lib/python2.6/site-packages/cv2.so
+#/usr/local/lib/python2.7/site-packages/cv2.so
+  
+############ For Python 3 ############
+## binary installed in dist-packages
+#/usr/local/lib/python3.5/dist-packages/cv2.cpython-35m-x86_64-linux-gnu.so
+#/usr/local/lib/python3.6/dist-packages/cv2.cpython-36m-x86_64-linux-gnu.so
+## binary installed in site-packages
+#/usr/local/lib/python3.5/site-packages/cv2.cpython-35m-x86_64-linux-gnu.so
+#/usr/local/lib/python3.6/site-packages/cv2.cpython-36m-x86_64-linux-gnu.so
+
+#Double check the exact path on your machine before running the following 
