058b4fa5baa306a5b1ada9082cbc644fb16afec7
Chibuike Okpaluba <chibike@users.noreply.github.com>
2019-06-08 20:01:19 +0100

    Update ubuntu_essentials.sh

--- ubuntu_essentials.sh
+++ ubuntu_essentials.sh
@@ -6,10 +6,5 @@ sudo apt install software-properties-common apt-transport-https
 sudo apt-get install git-core
 sudo apt install python3.6 python3-pip
 
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code
-
 pip3 install virtualenv virtualenvwrapper
 pip3 install numpy scipy matplotlib
